87c542af333b0b1ffacb5fba361100d16f0e300f
Shane B <shane@wander.dev>
2025-04-13 22:18:49 +0200

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 2
-        versionName = "1.0.1"
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
