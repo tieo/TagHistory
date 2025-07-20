3514e5811a0b2f6f370cc2d1c54b79963fc7db6a
Shane B <shane@wander.dev>
2025-07-20 22:22:16 +0200

    Refine the build setup and set up signed builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,18 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("KEYSTORE_FILE") ?: "release-keystore.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("release")
             isDebuggable = false
             isMinifyEnabled = false
             proguardFiles(
